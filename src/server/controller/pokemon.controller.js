be81d4fd6e425defb5403b8e2ce979f0b4787afc
Emk0oo <emir.j2002@gmail.com>
2024-01-20 17:12:34 +0100

    réorganisation fichier

---
+++ src/server/controller/pokemon.controller.js
@@ -0,0 +1,50 @@
+exports.getAllPokemon = (req, res) => {
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+};
+
+exports.getPokemonById = (req, res) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+};
+
+exports.getPokemonByName = (req, res) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${req.params.name}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+};
+
+exports.getRandomPokemon = (req, res) => {
+    let random = Math.floor(Math.random() * 151) + 1;
+    console.log(random);
+    const url = `https://pokeapi.co/api/v2/pokemon/${random}` ;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            res.json(data);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+};
