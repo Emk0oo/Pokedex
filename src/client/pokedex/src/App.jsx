9dbc9746e8ab4f43a7ab8d72f561bb99f113de52
Emk0oo <emir.j2002@gmail.com>
2024-01-21 20:20:55 +0100

    [ADD] Search bar

--- src/client/pokedex/src/App.jsx
+++ src/client/pokedex/src/App.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import logo from "./assets/pokeballSearch.png";
 
 const App = () => {
   console.log("App.jsx");
-  // function searchPokemonByID() {
-  //   console.log("searchPokemon");
-  //   let id = document.getElementById("idPkmn").value;
-  //   console.log(id);
-  //   fetch("https://pokeapi.co/api/v2/pokemon/" + id)
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data.sprites.front_default));
-  // }
+  function searchPokemonByID() {
+    console.log("searchPokemon");
+    let id = document.getElementById("idPkmn").value;
+    console.log(id);
+    fetch("https://pokeapi.co/api/v2/pokemon/" + id)
+      .then((response) => response.json())
+      .then((data) => console.log(data.sprites.front_default));
+  }
   return (
-    <div className="p-6 space-y-4 ">
+    <div className="px-20 py-10 space-y-4 ">
       <h1 className="text-4xl text-center space-y-4">Pokedex</h1>
+      <div className="flex ">
+        <input
+          className="w-full  rounded-2xl bg-slate-100 p-2"
+          placeholder="Search for a pokemon"
+          id="idPkmn"
+        ></input>
+        <a href="#" onClick={searchPokemonByID}>
+          <img src={logo} className="w-15 h-10 pl-2" draggable="false"></img>
+        </a>
+      </div>
       <div className="gap-6 grid sm:grid-cols-2">
         <a
           href="/pokemon"
@@ -36,7 +47,7 @@ const App = () => {
           href="/moves"
           className="border bg-yellow-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-yellow-400"
         >
-          Moves 
+          Moves
         </a>
       </div>
 
@@ -53,7 +64,7 @@ const App = () => {
             </a>
             <div className="text-2xl font-bold pt-2  ">#018</div>
           </div>
-          
+
           <ul className="flex flex-wrap mt-3 space-x-4">
             <li className="border rounded-xl px-4 py-1 text-sm bg-purple-400">
               Type1
@@ -62,7 +73,6 @@ const App = () => {
               Type2
             </li>
           </ul>
-
 
           <div className="flex flex-row mt-8 items-center md:mt-0 flex-wrap justify-around relative">
             <p className="max-w-lg text-lg sm:text-2xl ml-4">
