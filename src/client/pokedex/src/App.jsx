27aec530b6b0120432462e3c33d6ef8272624654
Emk0oo <emir.j2002@gmail.com>
2024-01-21 17:55:49 +0100

    [ADD] Random pokemon card

--- src/client/pokedex/src/App.jsx
+++ src/client/pokedex/src/App.jsx
@@ -12,15 +12,68 @@ const App = () => {
   // }
   return (
     <div className="p-6 space-y-4 ">
-      <div>
-        <h1 className="text-4xl text-center space-y-4">Pokedex</h1>
-        <div className="gap-6 grid sm:grid-cols-2">
-          <a href="/pokemon" className="border bg-green-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-green-400">Pokemon</a>
-          <a href="/abilities" className="border bg-red-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-red-400">Abilities</a>
-          <a href="/items" className="border bg-blue-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-blue-400">Items</a>
-          <a href="/moves" className="border bg-yellow-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-yellow-400">Moves</a>
+      <h1 className="text-4xl text-center space-y-4">Pokedex</h1>
+      <div className="gap-6 grid sm:grid-cols-2">
+        <a
+          href="/pokemon"
+          className="border bg-green-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-green-400"
+        >
+          Pokemon
+        </a>
+        <a
+          href="/abilities"
+          className="border bg-red-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-red-400"
+        >
+          Abilities
+        </a>
+        <a
+          href="/items"
+          className="border bg-blue-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-blue-400"
+        >
+          Items
+        </a>
+        <a
+          href="/moves"
+          className="border bg-yellow-500 text-white text-left sm:text-center overflow-hidden shadow-md rounded-xl p-6 hover:bg-yellow-400"
+        >
+          Moves 
+        </a>
+      </div>
+
+      <div className="flex  flex-wrap overflow-hidden">
+        <div className="border bg-purple-500 text-white shadow-md rounded-xl p-6 w-full">
+          <h2 className="text-left font-bold">Title : Random</h2>
+
+          <div className="flex flex-wrap w-full justify-between">
+            <a
+              href="/name"
+              className="hover:bg-purple-400 text-left text-4xl font-bold"
+            >
+              Bulbasaur
+            </a>
+            <div className="text-2xl font-bold pt-2  ">#018</div>
+          </div>
+          
+          <ul className="flex flex-wrap mt-3 space-x-4">
+            <li className="border rounded-xl px-4 py-1 text-sm bg-purple-400">
+              Type1
+            </li>
+            <li className="border rounded-xl px-4 py-1 text-sm bg-purple-400">
+              Type2
+            </li>
+          </ul>
+
+
+          <div className="flex flex-row mt-8 items-center md:mt-0 flex-wrap justify-around relative">
+            <p className="max-w-lg text-lg sm:text-2xl ml-4">
+              Description blabla
+            </p>
+            <img
+              className=" relative w-auto h-64 sm:h-80 "
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            ></img>
+          </div>
         </div>
-        {/*  overflow-hidden shadow-md rounded-xl p-3  */}
       </div>
     </div>
   );
